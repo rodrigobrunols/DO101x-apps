023fb53e809d7bd230aff7ed0c2a7e8aa0da26ab
rodrigo.leoncio <rodrigo.leoncio@sicoob.com.br>
2026-07-02 18:15:34 -0300

    created DB_USER and DB_PASSWORD variable

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+const DB_USER = process.env.DB_USER || 'contacts';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'contacts';
+
+// Dynamically construct the default connection string using template literals
+const DB_CONFIG = process.env.DB_CONFIG || `postgresql://${DB_USER}:${DB_PASSWORD}@localhost:5432/contacts`;
+
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
     ssl: false,
 });
-  
-module.exports = { pgconn }
+
+module.exports = { pgconn };
